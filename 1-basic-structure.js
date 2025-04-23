0886a14a8a68191d5315d998dd880311f0f5bea1
flaviare1s <flaviarster@gmail.com>
2025-04-22 22:28:32 -0300

    Exercícios aula 22/04/25

--- 1-basic-structure.js
+++ 1-basic-structure.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")();
+
 // // let giria = 'Mané';
 
 // // console.log("Hello World!")
@@ -66,7 +68,6 @@
 // console.log(autenticacao)
 
 // Exercício 1 - Verificar se é par ou ímpar
-
 let num = 23
 
 if(num % 2 === 0) {
@@ -78,7 +79,6 @@ if(num % 2 === 0) {
 }
 
 // Exercício 2 - Encontrar o maior entre 3 números
-
 let num1 = 44
 let num2 = 8
 let num3 = 16
@@ -95,3 +95,45 @@ if(num3 > maior) {
 }
 
 console.log(maior)
+
+// Exercício 3 - Verificar se um número é positivo, negativo ou zero
+let valor = 2
+
+if (valor > 0) {
+  console.log(`O número ${valor} é positivo!`)
+} else if (valor < 0) {
+  console.log(`O número ${valor} é negativo!`);
+} else {
+  console.log('É zero!')
+}
+
+// Exercício 4 - Verificar se um ano é bissexto
+let ano = 2020
+
+if (ano % 400 === 0) {
+  console.log(`${ano} é bissexto`);
+} else if (ano % 100 === 0) {
+  console.log(`${ano} não é bissexto`);
+} else if (ano % 4 === 0) {
+  console.log(`${ano} é bissexto`);
+} else {
+  console.log(`${ano} não é bissexto`);
+}
+
+//Exercício 5 -  Definam faixas etárias para crianças, adolescentes e adultos e com switch apresentem na tela se a pessoa que informou a idade está em uma e qual a faixa etária
+let idade = Number(prompt('Qual a sua idade? '))
+
+switch (true) {
+  case idade < 12:
+    console.log("Criança");
+    break;
+  case idade < 18:
+    console.log("Adolescente");
+    break;
+  case idade < 60:
+    console.log("Adulto");
+    break;
+  case idade >= 60:
+    console.log("Idoso");
+    break;
+}
